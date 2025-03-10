053098bd59f772ba345fe809516f2fac1fbee6cf
szymon <szymon.pt4sznik@gmail.com>
2025-03-10 22:14:10 +0100

    created draw method in game screen class, changed fitviewport to stretch viewport, rendered background texture on screen

--- core/src/main/java/io/github/badgersky/BonesAndDice/GameScreen.java
+++ core/src/main/java/io/github/badgersky/BonesAndDice/GameScreen.java
@@ -1,17 +1,38 @@
 package io.github.badgersky.BonesAndDice;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
     final Main game;
 
+    Texture background;
+
     public GameScreen(final Main game) {
         this.game = game;
+
+        background = new Texture("background_game.png");
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+        game.batch.begin();
+
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+
+        game.batch.draw(background, 0, 0, worldWidth, worldHeight);
+
+        game.batch.end();
     }
 
     @Override
     public void render(float v) {
-
+        draw();
     }
 
     @Override
@@ -21,7 +42,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int i, int i1) {
-
+        game.viewport.update(i, i1, true);
     }
 
     @Override
--- core/src/main/java/io/github/badgersky/BonesAndDice/Main.java
+++ core/src/main/java/io/github/badgersky/BonesAndDice/Main.java
@@ -4,20 +4,19 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     public SpriteBatch batch;
     public BitmapFont font;
-    public FitViewport viewport;
+    public StretchViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
-        viewport = new FitViewport(12, 7.5f);
+        viewport = new StretchViewport(12, 7.5f);
 
         font.setUseIntegerPositions(false);
         font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
