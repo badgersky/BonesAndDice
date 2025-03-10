f20f91e49ea056a88ae7c2240a03d9853df1bb06
szymon <szymon.pt4sznik@gmail.com>
2025-03-10 20:17:21 +0100

    updated main game class, initialized font, viewport and spritebatch

--- core/src/main/java/io/github/badgersky/BonesAndDice/Main.java
+++ core/src/main/java/io/github/badgersky/BonesAndDice/Main.java
@@ -1,34 +1,38 @@
 package io.github.badgersky.BonesAndDice;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
     private SpriteBatch batch;
-    private Texture image;
+    private BitmapFont font;
+    private FitViewport viewport;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+        viewport = new FitViewport(8, 5);
+
+        font.setUseIntegerPositions(false);
+        font.getData().setScale((float)viewport.getScreenHeight() / Gdx.graphics.getHeight());
+
+        this.setScreen(new MainMenu());
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        super.render();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        font.dispose();
     }
 }
