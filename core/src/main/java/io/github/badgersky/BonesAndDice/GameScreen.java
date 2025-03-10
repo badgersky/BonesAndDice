938e1712ece4f32a83ce129e78d1c0f5f54f7755
szymon <szymon.pt4sznik@gmail.com>
2025-03-10 20:45:25 +0100

    game screen class, initialization of game object inside the class

---
+++ core/src/main/java/io/github/badgersky/BonesAndDice/GameScreen.java
@@ -0,0 +1,46 @@
+package io.github.badgersky.BonesAndDice;
+
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+    final Main game;
+
+    public GameScreen(final Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
