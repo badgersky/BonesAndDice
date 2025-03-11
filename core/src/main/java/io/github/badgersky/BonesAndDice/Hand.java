e3724f859b215fedae92adf7c952a0fc32ff2697
szymon <szymon.pt4sznik@gmail.com>
2025-03-11 03:20:03 +0100

    Hand class, class constructor

---
+++ core/src/main/java/io/github/badgersky/BonesAndDice/Hand.java
@@ -0,0 +1,21 @@
+package io.github.badgersky.BonesAndDice;
+
+import java.util.ArrayList;
+
+public class Hand {
+
+    public ArrayList<Dice> dices;
+    public ArrayList<Dice> selectedDices;
+    public Hand() {
+        selectedDices = new ArrayList<>();
+        dices = new ArrayList<>();
+        Dice newDice;
+
+        for (int i = 0; i < 6; i++) {
+            newDice = new Dice();
+            if (dices != null) {
+                dices.add(newDice);
+            }
+        }
+    }
+}
