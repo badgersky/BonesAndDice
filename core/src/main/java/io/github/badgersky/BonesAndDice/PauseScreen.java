ca6700e1a7434b4b36cc19a3c4b64bc1c2c59898
szymon <szymon.pt4sznik@gmail.com>
2025-06-02 23:59:10 +0200

    created pause screen class

---
+++ core/src/main/java/io/github/badgersky/BonesAndDice/PauseScreen.java
@@ -0,0 +1,46 @@
+package io.github.badgersky.BonesAndDice;
+
+import com.badlogic.gdx.Screen;
+
+public class PauseScreen implements Screen {
+    final Main game;
+
+    public PauseScreen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
