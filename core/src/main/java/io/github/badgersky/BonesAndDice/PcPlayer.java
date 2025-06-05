6785c878e2e0411ad14aebef74ffbcf3a82fcd29
szymon <szymon.pt4sznik@gmail.com>
2025-06-05 05:23:40 +0200

    method that chooses the best combination of dices from hand of PcPlayer, tests for the method

--- core/src/main/java/io/github/badgersky/BonesAndDice/PcPlayer.java
+++ core/src/main/java/io/github/badgersky/BonesAndDice/PcPlayer.java
@@ -12,9 +12,13 @@ public class PcPlayer {
     }
 
     public int play() {
+        List<Dice> bestChoice;
+
         if (hand.hasNoPoints()) {
             return -1;
         }
+
+        bestChoice = chooseBestCombo();
 
         return 1;
     }
@@ -41,6 +45,39 @@ public class PcPlayer {
         return subsets;
     }
 
-    private void chooseBestCombo() {
+    public List<Dice> chooseBestCombo() {
+        List<List<Dice>> subsets = generateSubsets();
+        List<List<Dice>> bestCombos = new ArrayList<>();
+        List<Dice> bestCombo;
+        int points;
+        int maxPoints = 0;
+
+        for (List<Dice> subset : subsets) {
+            for (Dice d : subset) {
+                d.markSelected();
+            }
+
+            points = hand.countPoints();
+            if (points > maxPoints) {
+                bestCombos.clear();
+                maxPoints = points;
+                bestCombos.add(subset);
+            } else if (points == maxPoints) {
+                bestCombos.add(subset);
+            }
+
+            for (Dice d : subset) {
+                d.markUnselected();
+            }
+        }
+
+        bestCombo = bestCombos.get(0);
+        for (List<Dice> combo : bestCombos) {
+            if (combo.size() < bestCombo.size()) {
+                bestCombo = combo;
+            }
+        }
+
+        return bestCombo;
     }
 }
--- core/src/test/java/io/github/badgersky/BonesAndDice/PcPlayerTest.java
+++ core/src/test/java/io/github/badgersky/BonesAndDice/PcPlayerTest.java
@@ -34,4 +34,103 @@ public class PcPlayerTest {
 
         assertEquals(63, subsets.size());
     }
+
+    @Test
+    void testChooseBestCombo1() {
+        Hand hand = new Hand();
+        PcPlayer player = new PcPlayer(hand);
+        List<Dice> bestCombo;
+
+        hand.dices.get(0).setValue(2);
+        hand.dices.get(1).setValue(2);
+        hand.dices.get(2).setValue(2);
+        hand.dices.get(3).setValue(3);
+        hand.dices.get(4).setValue(4);
+        hand.dices.get(5).setValue(4);
+
+        bestCombo = player.chooseBestCombo();
+
+        assertEquals(3, bestCombo.size());
+        assertEquals(2, bestCombo.get(0).getValue());
+        assertEquals(2, bestCombo.get(1).getValue());
+        assertEquals(2, bestCombo.get(2).getValue());
+    }
+
+    @Test
+    void testChooseBestCombo2() {
+        Hand hand = new Hand();
+        PcPlayer player = new PcPlayer(hand);
+        List<Dice> bestCombo;
+
+        hand.dices.get(0).setValue(1);
+        hand.dices.get(1).setValue(2);
+        hand.dices.get(2).setValue(2);
+        hand.dices.get(3).setValue(3);
+        hand.dices.get(4).setValue(4);
+        hand.dices.get(5).setValue(4);
+
+        bestCombo = player.chooseBestCombo();
+
+        assertEquals(1, bestCombo.size());
+        assertEquals(1, bestCombo.get(0).getValue());
+    }
+
+    @Test
+    void testChooseBestCombo3() {
+        Hand hand = new Hand();
+        PcPlayer player = new PcPlayer(hand);
+        List<Dice> bestCombo;
+
+        hand.dices.get(0).setValue(1);
+        hand.dices.get(1).setValue(1);
+        hand.dices.get(2).setValue(1);
+        hand.dices.get(3).setValue(1);
+        hand.dices.get(4).setValue(1);
+        hand.dices.get(5).setValue(1);
+
+        bestCombo = player.chooseBestCombo();
+
+        assertEquals(6, bestCombo.size());
+        assertEquals(1, bestCombo.get(0).getValue());
+        assertEquals(1, bestCombo.get(5).getValue());
+    }
+
+    @Test
+    void testChooseBestCombo4() {
+        Hand hand = new Hand();
+        PcPlayer player = new PcPlayer(hand);
+        List<Dice> bestCombo;
+
+        hand.dices.get(0).setValue(1);
+        hand.dices.get(1).setValue(2);
+        hand.dices.get(2).setValue(3);
+        hand.dices.get(3).setValue(4);
+        hand.dices.get(4).setValue(5);
+        hand.dices.get(5).setValue(6);
+
+        bestCombo = player.chooseBestCombo();
+
+        assertEquals(6, bestCombo.size());
+        assertEquals(1, bestCombo.get(0).getValue());
+        assertEquals(6, bestCombo.get(5).getValue());
+    }
+
+    @Test
+    void testChooseBestCombo5() {
+        Hand hand = new Hand();
+        PcPlayer player = new PcPlayer(hand);
+        List<Dice> bestCombo;
+
+        hand.dices.get(0).setValue(5);
+        hand.dices.get(1).setValue(3);
+        hand.dices.get(2).setValue(3);
+        hand.dices.get(3).setValue(4);
+        hand.dices.get(4).setValue(6);
+        hand.dices.get(5).setValue(6);
+
+        bestCombo = player.chooseBestCombo();
+
+        assertEquals(1, bestCombo.size());
+        assertEquals(5, bestCombo.get(0).getValue());
+    }
 }
