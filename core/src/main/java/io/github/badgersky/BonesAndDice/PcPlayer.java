3d40ef4d1c8b36262267515524bab2be903fb994
szymon <szymon.pt4sznik@gmail.com>
2025-06-05 03:53:27 +0200

    added method generating all subsets of hand.dices array in PcPlayer

--- core/src/main/java/io/github/badgersky/BonesAndDice/PcPlayer.java
+++ core/src/main/java/io/github/badgersky/BonesAndDice/PcPlayer.java
@@ -1,10 +1,46 @@
 package io.github.badgersky.BonesAndDice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PcPlayer {
 
     Hand hand;
 
     public PcPlayer(Hand hand) {
         this.hand = hand;
+    }
+
+    public int play() {
+        if (hand.hasNoPoints()) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+    private List<List<Dice>> generateSubsets() {
+        List<List<Dice>> subsets = new ArrayList<>();
+
+        for (Dice d : hand.dices) {
+            List<List<Dice>> tmpSubsets = new ArrayList<>();
+
+            for (List<Dice> existing : subsets) {
+                List<Dice> newSubset = new ArrayList<>(existing);
+                newSubset.add(d);
+                tmpSubsets.add(newSubset);
+            }
+
+            List<Dice> tmp = new ArrayList<>();
+            tmp.add(d);
+            tmpSubsets.add(tmp);
+
+            subsets.addAll(tmpSubsets);
+        }
+
+        return subsets;
+    }
+
+    private void chooseBestCombo() {
     }
 }
